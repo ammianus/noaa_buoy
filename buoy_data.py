574e00499af13bfb3bca2c3f5cd6d89b645867e4
ammianus <null>
2019-09-04 07:14:28 -0400

    windspeed and water temp

--- buoy_data.py
+++ buoy_data.py
@@ -5,7 +5,7 @@ import sys
 DEFAULT_BUOY = 44011
 
 #calculate the current air temp at the station and it's relation to the max temp
-def airTempStatus(buoydata):
+def airTempStatus(buoydata: pd.DataFrame) -> str:
     airTemp = buoydata.at[0,'ATMP']
     date = '{}-{}-{} {}:{} UTC'.format(buoydata.at[0,'#YY'],buoydata.at[0,'MM'],buoydata.at[0,'DD'],buoydata.at[0,'hh'],buoydata.at[0,'mm'])
     print('{} Air Temp {}'.format(date,airTemp))
@@ -25,7 +25,47 @@ def airTempStatus(buoydata):
     print(text)
     return text
 
-def fetchRealTime2Data(theBuoyId):
+def waterTempStatus(buoydata: pd.DataFrame) -> str:
+    waterTemp = buoydata.at[0,'WTMP']
+    date = '{}-{}-{} {}:{} UTC'.format(buoydata.at[0,'#YY'],buoydata.at[0,'MM'],buoydata.at[0,'DD'],buoydata.at[0,'hh'],buoydata.at[0,'mm'])
+    print('{} Water Temp {}'.format(date,waterTemp))
+    calcs = buoydata.agg({'WTMP' : ['median','min','max']})#, 'WTMP' : ['median','min', 'max'], 'WSPD' : ['median','min', 'max']})
+    print(calcs)
+    maxTemp = calcs.at['max','WTMP']
+
+    maxTempRelativeDesc = ''    
+    if waterTemp == maxTemp:
+        maxTempRelativeDesc = 'equal to'
+        pass
+    elif waterTemp < maxTemp:
+        maxTempRelativeDesc = 'lower than'
+        pass
+
+    text = 'The latest water temperature at Station {} is {}°C and is {} the maximum temperature for the past 45 days of {}°C.'.format(buoyId, waterTemp,maxTempRelativeDesc,maxTemp)
+    print(text)
+    return text
+
+def windSpeedStatus(buoydata: pd.DataFrame) -> str:
+    windSpeed = buoydata.at[0,'WSPD']
+    date = '{}-{}-{} {}:{} UTC'.format(buoydata.at[0,'#YY'],buoydata.at[0,'MM'],buoydata.at[0,'DD'],buoydata.at[0,'hh'],buoydata.at[0,'mm'])
+    print('{} Water Temp {}'.format(date,windSpeed))
+    calcs = buoydata.agg({'WSPD' : ['median','min','max']})#, 'WTMP' : ['median','min', 'max'], 'WSPD' : ['median','min', 'max']})
+    print(calcs)
+    maxSpeed = calcs.at['max','WSPD']
+
+    maxTempRelativeDesc = ''    
+    if windSpeed == maxSpeed:
+        maxTempRelativeDesc = 'equal to'
+        pass
+    elif windSpeed < maxSpeed:
+        maxTempRelativeDesc = 'lower than'
+        pass
+
+    text = 'The latest wind speed at Station {} is {} m/s and is {} the maximum speed for the past 45 days of {} m/s.'.format(buoyId, windSpeed, maxTempRelativeDesc, maxSpeed)
+    print(text)
+    return text
+
+def fetchRealTime2Data(theBuoyId: int) -> pd.DataFrame:
     #realtime data for station 44011
     url = 'https://www.ndbc.noaa.gov/data/realtime2/{}.txt'.format(theBuoyId)
 
@@ -40,10 +80,10 @@ def fetchRealTime2Data(theBuoyId):
     #print(buoydata.describe())
     return buoydata
 
-def buoyLat():
+def buoyLat() -> float:
     return 41.070000
 
-def buoyLong():
+def buoyLong() -> float:
     return -66.588000
 
 #print('This is the name of the script: ', sys.argv[0])
@@ -58,10 +98,13 @@ else:
     pass
 
 #realtime data for station 44011
-#buoydata = fetchRealTime2Data(buoyId)
+buoydata = fetchRealTime2Data(buoyId)
 #print(buoydata.describe())
 
+#testing
 #airTempStatus(buoydata)
+#waterTempStatus(buoydata)
+#windSpeedStatus(buoydata)
 
 
 
--- twitter_client.py
+++ twitter_client.py
@@ -1,25 +1,57 @@
 from TwitterAPI import TwitterAPI
 from twitter_creds import *
+from pandas import DataFrame
 import buoy_data
 import pprint
 
+#init TwitterAPI with your own credentials
 api = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret)
 
 #get new data for buoy 44011
 data = buoy_data.fetchRealTime2Data(44011)
 
-#get status for air temps
-newstatus = buoy_data.airTempStatus(data)
+#general post tweet
+def postTweet(updateContent: dict) -> None:
+    r = api.request('statuses/update', updateContent)
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(r.json()) 
+    pass
 
-#post tweet
-r = api.request('statuses/update', 
-    {'status': newstatus, 
-    'lat': buoy_data.buoyLat(), 
-    'long': buoy_data.buoyLong(), })
+#get status for air temps and post it
+def airTempTweet(data: DataFrame) -> None:
+    newstatus = buoy_data.airTempStatus(data)
+    updateContent = {'status': newstatus, 'lat': buoy_data.buoyLat(), 'long': buoy_data.buoyLong(), }
+    postTweet(updateContent)
+    pass
 
+def waterTempTweet(data: DataFrame) -> None:
+    newstatus = buoy_data.waterTempStatus(data)+' #HurricaneDorian'
+    updateContent = {'status': newstatus, 'lat': buoy_data.buoyLat(), 'long': buoy_data.buoyLong(), }
+    postTweet(updateContent)
+    pass
+
+def windSpeedTweet(data: DataFrame) -> None:
+    newstatus = buoy_data.windSpeedStatus(data)+' #HurricaneDorian'
+    updateContent = {'status': newstatus, 'lat': buoy_data.buoyLat(), 'long': buoy_data.buoyLong(), }
+    postTweet(updateContent)
+    pass
+
+def postStationInfo() -> None:
+    newstatus = 'Station 44011 (LLNR 825) - GEORGES BANK 170 NM East of Hyannis, MA\nOwned and maintained by National Data Buoy Center\n3-meter foam buoy\nSCOOP payload\n41°4\'12\" N 66°35\'16\" W\nhttps://www.ndbc.noaa.gov/station_page.php?station=44011'
+    updateContent = {'status': newstatus, 'lat': buoy_data.buoyLat(), 'long': buoy_data.buoyLong(), }
+    postTweet(updateContent)
+    pass
+
+def postGeneralMessage(message: str) -> None:
+    updateContent = {'status': message, 'lat': buoy_data.buoyLat(), 'long': buoy_data.buoyLong(), }
+    postTweet(updateContent)
+    pass
+
+#postGeneralMessage('NOAA Buoy 44011 is testing some new python functions tonight. Prepping for #HurricaneDorian later this week.')
+waterTempTweet(data)
+windSpeedTweet(data)
+
+#geo/search not returning anything for oceanic Lat/Long locations?
 #r = api.request('geo/search', {'lat': buoy_data.buoyLat(), 
 #    'long': buoy_data.buoyLong()
 #    },None,'GET')
-
-pp = pprint.PrettyPrinter(indent=4)
-pp.pprint(r.json()) 
